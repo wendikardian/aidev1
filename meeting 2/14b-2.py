5670c339c52b199863b02133290503c0139cc141
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-09-07 19:42:34 +0700

    14b-2

---
+++ meeting 2/14b-2.py
@@ -0,0 +1,54 @@
+
+
+# # print(fullName1)
+
+
+# # names = ['Cleo', 'Wendi', 'James', 'Jeniffer', 29, 9.2, True]
+# names = ['Cleo', 'Wendi', 'James', 'Jeniffer', 'David', 'George', 'Thompson']
+# # print(names)
+# # print(names[2])
+# # print(names[-1])
+
+# names[2] = 'steve'  # reassign list / changes the element
+
+# names.append('Taylor')  # add new element to list
+
+# # print(names)
+
+# # print(len(names))
+# # names.remove('Wendi') #
+# # print(len(names))
+
+# print(names)
+# print(names[3:6])
+# print(names[2:])
+# print(names[: 4])
+
+# # create list of numbers random 1 - 100
+# num = [ 90, 78, 22, 100, 87, 55, 98]
+# # sort desc
+# num.sort(reverse=True)
+# print(num)
+
+
+# num = [[1, 2, 3, 4, 5],
+#        [5, 6, 7, 8, 9],
+#        [10, 11, 12, 13, 14]]
+
+# print(num[1][4])
+
+# names = ('Loui', 'Wendi', 'James')
+# # names.append('Cleo')
+# print(names[1])
+
+
+movies = {
+    'title' : 'Avangers Endgame',
+    'year' : 2019,
+    'genre' : 'Action',
+    'rating' : 8.9,
+    'director' : 'Russo Brothers',
+    'actors' : ['Robert Downey Jr', 'Chris Evans', 'Chris Hemsworth', 'Scarlett Johansson', 'Mark Ruffalo']
+}
+
+print(movies['actors'][3])
